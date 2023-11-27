567950e92619a1daeb6707106cd3d8a3c76c1ca4
ayana-kubo <107912254+ayana-kubo@users.noreply.github.com>
2023-11-27 14:59:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// JavaScript
+const open = document.querySelector('#btn-open');
+const close = document.querySelector('#btn-close');
+const menu = document.querySelector('#menu');
+const items = document.querySelectorAll('.item');
+
+const options = {
+  duration: 600,
+  easing: 'ease',
+  fill: 'forwards',
+}
+
+
+const menuOpen = () => {
+  const keyframes = {
+    visibility: ['hidden','visible'],
+    opacity: [0,1]
+  }
+  menu.animate(keyframes,options);
+
+  items.forEach((item,index) => {
+    item.animate({opacity:[0,1]},{
+      duration: 600,
+      easing: 'ease',
+      fill: 'forwards',
+      delay: index * 200,
+    });
+  });
+
+}
+open.addEventListener('click',menuOpen);
+
+const menuClose = () => {
+  const keyframes = {
+    visibility: ['visible','hidden'],
+    opacity: [1,0]
+  }
+  menu.animate(keyframes,options);
+
+  items.forEach((item) => {
+    item.animate({opacity:[1,0]},{
+      duration: 600,
+      easing: 'ease',
+      fill: 'forwards',
+    });
+  });
+
+}
+close.addEventListener('click',menuClose);
+console.log('Hello world!');
+
+// 色が変わる
+const color = document.querySelector('.color');
+
+const colorKeyframes = {
+  backgroundColor: ['orange ','yellow ','lightgreen','skyblue'],
+}
+
+const colorOptions = {
+  duration: 5000,
+  direction: 'alternate',
+  iterations: Infinity,
+}
+
+color.animate(colorKeyframes,colorOptions);
+
